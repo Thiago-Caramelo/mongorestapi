1fe81bf8a936906617d9ead998b945bea8005da5
Thiago Caramelo <thiago.pescado@gmail.com>
2019-06-18 21:53:19 -0300

    feat(mongo): pool

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,19 +1,18 @@
+const debug = require('debug')('pet:mongo');
 const { MongoClient, Db } = require('mongodb');
 
 const url = 'mongodb://localhost:27017';
 let mongodb = Db.prototype;
 
-const init = async () => {
-  try {
-    const mongoClient = await MongoClient.connect(url, {
-      poolSize: 10,
-    });
-    mongodb = mongoClient.db('pet');
-  } catch (error) {
-    process.exit(-1);
-  }
-};
-init();
+MongoClient.connect(url, {
+  poolSize: 10,
+  useNewUrlParser: true,
+}).then((client) => {
+  mongodb = client.db('pet');
+}).catch((error) => {
+  debug(error);
+  process.exit(error.code || -1);
+});
 
 module.exports = {
   mongodb,
